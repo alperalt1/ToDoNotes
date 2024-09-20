e62f1b1d395cfdaa1353d418c5a4c781eaf87681
alperalt1 <alperalt@espol.edu.ec>
2024-09-20 10:25:51 -0500

    solucion rutas hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,40 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import Passwordrecovery from "./pages/passwordrecovery";
+import { InitPage } from "./pages/initpage";
+import Passwordupdate from "./pages/passwordupdate";
+import Loginpage from "./pages/loginpage";
+import Registerpage from "./pages/registerpage";
+const router =createHashRouter([
+  {
+    basename: "/ToDoNotes"
+  },
+  {
+    path:"/", 
+    element:<Loginpage />,
+  },
+  {
+    path:"/register", 
+    element:<Registerpage />,
+  },
+  {
+    path:"/passwordrecovery", 
+    element:<Passwordrecovery />,
+  },
+  {
+    path:"/init", 
+    element:<InitPage />,
+  },
+  {
+    path:"/passwordupdate", 
+    element:<Passwordupdate/>,
+  },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
