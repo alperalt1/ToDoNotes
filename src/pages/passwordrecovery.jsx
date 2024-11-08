c5cb398900914598ed1e806a36a26a7cb237e351
alperalt1 <alperalt@espol.edu.ec>
2024-11-08 11:08:54 -0500

    solucion password c

--- src/pages/passwordrecovery.jsx
+++ src/pages/passwordrecovery.jsx
@@ -13,7 +13,7 @@ const Passwordrecovery = () => {
     try {
       const { data, error } = await supabase.auth
       .resetPasswordForEmail(info.email,{
-        redirectTo: 'https://alperalt1.github.io/ToDoNotes/passwordupdate',
+        redirectTo: 'https://alperalt1.github.io/ToDoNotes/#/passwordupdate',
       });
       if (error==null) {
         Swal.fire("Se enviara un enlace de restablecimiento de contraseña al correo electrónico del usuario");
